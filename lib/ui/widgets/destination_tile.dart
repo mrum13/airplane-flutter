1ffc166f82b640c33eacf979cb50e15e7809dc31
mrum13 <muhammadrum135@gmail.com>
2022-02-22 09:30:17 +0800

    detail page, background image

--- lib/ui/widgets/destination_tile.dart
+++ lib/ui/widgets/destination_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_airplane/shared/theme.dart';
+import 'package:flutter_airplane/ui/pages/detail_page.dart';
 
 class DestinationTile extends StatelessWidget {
 
@@ -17,65 +18,75 @@ class DestinationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(top: 16),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: kWhiteColor,
-          borderRadius: BorderRadius.circular(18),
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: 70,
-              height: 70,
-              margin: EdgeInsets.only(right: 16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(imageUrl))),
-            ),
-            Expanded(
-              child: Column(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
+          ),
+        );
+      },
+      child: Container(
+          margin: EdgeInsets.only(top: 16),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(imageUrl))),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: blackTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(city,
+                        style: greyTextStyle.copyWith(fontWeight: light))
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Container(
+                    width: 20,
+                    height: 20,
+                    margin: EdgeInsets.only(right: 2),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/icon_star.png'),
+                      ),
+                    ),
+                  ),
                   Text(
-                    name,
+                    rating.toString(),
                     style: blackTextStyle.copyWith(
-                      fontSize: 18,
                       fontWeight: medium,
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(city,
-                      style: greyTextStyle.copyWith(fontWeight: light))
+                  )
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 20,
-                  height: 20,
-                  margin: EdgeInsets.only(right: 2),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/icon_star.png'),
-                    ),
-                  ),
-                ),
-                Text(
-                  rating.toString(),
-                  style: blackTextStyle.copyWith(
-                    fontWeight: medium,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
