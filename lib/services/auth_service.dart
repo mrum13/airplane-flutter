67c802cbb2e5f538b2522f421bfc684ad05fb3e6
mrum13 <muhammadrum135@.com>
2022-07-16 18:57:52 +0800

    Add SignUp Auth Service & User Service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_airplane/models/user_model.dart';
+import 'package:flutter_airplane/services/user_service.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserModel> signUp(
+      {required String email,
+      required String password,
+      required String name,
+      String hobby = ''}) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      UserModel user = UserModel(
+          id: userCredential.user!.uid,
+          email: email,
+          name: name,
+          hobby: hobby,
+          balance: 280000000);
+
+      await UserService().setUser(user);
+
+      return user;
+    } catch (e) {}
+  }
+}
