c466300d835a8a6848a44c32f6162c01b6fca841
mrum13 <muhammadrum135@.com>
2022-07-16 12:54:15 +0800

    Create Page Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airplane/cubit/page_cubit.dart';
 import 'package:flutter_airplane/ui/pages/bonus_page.dart';
 import 'package:flutter_airplane/ui/pages/get_started_page.dart';
 import 'package:flutter_airplane/ui/pages/main_page.dart';
 import 'package:flutter_airplane/ui/pages/sign_up_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'ui/pages/splash_page.dart';
 
@@ -11,19 +13,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => PageCubit())],
+      child: MaterialApp(debugShowCheckedModeBanner: false, routes: {
         '/': (context) => SplashPage(),
         '/get-started': (context) => GetStartedPage(),
-        '/sign-up' : (context) => SignUpPage(),
-        '/bonus' : (context) => BonusPage(),
-        '/main' : (context) => MainPage(),
-      }
+        '/sign-up': (context) => SignUpPage(),
+        '/bonus': (context) => BonusPage(),
+        '/main': (context) => MainPage(),
+      }),
     );
   }
 }
